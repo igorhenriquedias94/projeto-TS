7978a663c74e2b1fba083af82d8469b77f228c54
Igor Dias <igorhenriquedias94@gmail.com>
2022-12-13 23:30:44 -0300

    add coverage

--- tests.py
+++ tests.py
@@ -1,9 +1,29 @@
-def capital_case(x):
-    return x.capitalize()
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
-    
-    
-def test_deps():
-    assert get_db()
+from business.database.database import Base
+from business.database.database import engine
+
+from api.endpoints.user import router as user_router
+from api.endpoints.item import router as item_router
+from api.endpoints.wallet import router as wallet_router
+from api.endpoints.product_balance import router as product_balance_router
+from api.endpoints.transaction import router as transaction_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def test_fast_api2():
+    assert app is not False
+
+    app.include_router(user_router, prefix="/user", tags=["user"])
+    app.include_router(item_router, prefix="/item", tags=["item"])
+    app.include_router(wallet_router, prefix="/wallet", tags=["wallet"])
+    app.include_router(product_balance_router, prefix="/product-balance", tags=["product-balance"])
+    app.include_router(transaction_router, prefix="/transaction", tags=["transaction"])
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
